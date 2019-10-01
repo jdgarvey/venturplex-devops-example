dbcc4f8b0ea731714f565fd8e5dfef286faccca2
Victor Avila <victor.avila@venturplex.com>
2019-10-01 11:22:50 -0700

    create: toolbar and users component shell

--- client/apps/users/src/app/app.module.ts
+++ client/apps/users/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '@client/material';
+import { UiToolbarModule } from '@client/ui-toolbar';
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UsersComponent],
   imports: [
     BrowserModule,
-    MaterialModule
+    MaterialModule,
+    UiToolbarModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
